8bdffd8fe7aad93b474c0b8e2307d26fae70e53e
manohar icon <manohar icon@LAPTOP-KFOVULAQ>
2023-02-28 11:42:26 +0530

    hii

---
+++ src/practice/LaunchBrowser.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class LaunchBrowser {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Hi selenium program");
+		System.out.println("hi");
+		System.out.println("Moksha");
+	}
+
+}
